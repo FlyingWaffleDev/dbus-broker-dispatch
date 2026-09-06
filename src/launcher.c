5b434ac5df453c4a5d4ab49cd7d8124c03df4528
FlyingWaffle <flyingwaffle@pm.me>
2026-09-06 15:45:05 -0600

    fixed for clan-format

--- src/launcher.c
+++ src/launcher.c
@@ -979,12 +979,18 @@ int main(int argc, char **argv)
         int option;
         const char *runtime, *configured_address;
         static const struct option options[] = {
-                {"scope", required_argument, NULL, 's'},    {"config-file", required_argument, NULL, 'c'},
-                {"address", required_argument, NULL, 'a'},  {"broker", required_argument, NULL, 'b'},
-                {"pid-file", required_argument, NULL, 'p'}, {"system-uid-max", required_argument, NULL, 'm'},
-                {"audit", no_argument, NULL, 'A'},          {"foreground", no_argument, NULL, 'f'},
-                {"fork", no_argument, NULL, 'F'},           {"ready-fd", required_argument, NULL, 'r'},
-                {"help", no_argument, NULL, 'h'},           {"version", no_argument, NULL, 'V'},
+                {"scope", required_argument, NULL, 's'},
+                {"config-file", required_argument, NULL, 'c'},
+                {"address", required_argument, NULL, 'a'},
+                {"broker", required_argument, NULL, 'b'},
+                {"pid-file", required_argument, NULL, 'p'},
+                {"system-uid-max", required_argument, NULL, 'm'},
+                {"audit", no_argument, NULL, 'A'},
+                {"foreground", no_argument, NULL, 'f'},
+                {"fork", no_argument, NULL, 'F'},
+                {"ready-fd", required_argument, NULL, 'r'},
+                {"help", no_argument, NULL, 'h'},
+                {"version", no_argument, NULL, 'V'},
                 {NULL, 0, NULL, 0},
         };
         launcher.system_uid_max = 999;
